b89fbcd50a4ed8b55114b57e16bcfc56ab045512
PiotrPn <bojki1975@gmail.com>
2026-06-30 20:21:54 +0200

    feat: add physical locale routes

---
+++ src/app/[locale]/oferta/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { notFound, redirect } from "next/navigation";
+import { defaultLocale, isLocale } from "@/lib/i18n/config";
+import { OfferPage } from "../../../_shared/OfferPage";
+
+type PageProps = {
+  params: Promise<{
+    locale: string;
+    id: string;
+  }>;
+};
+
+export default async function LocalizedOfferPage({ params }: PageProps) {
+  const { locale: rawLocale, id } = await params;
+
+  if (!isLocale(rawLocale)) {
+    notFound();
+  }
+
+  if (rawLocale === defaultLocale) {
+    redirect(`/oferta/${id}`);
+  }
+
+  return <OfferPage locale={rawLocale} offerId={id} />;
+}
---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,28 @@
+import { notFound, redirect } from "next/navigation";
+import { defaultLocale, isLocale } from "@/lib/i18n/config";
+import { HomePage } from "../_shared/HomePage";
+
+type PageProps = {
+  params: Promise<{ locale: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export default async function LocalizedPage({
+  params,
+  searchParams,
+}: PageProps) {
+  const [{ locale: rawLocale }, resolvedSearchParams] = await Promise.all([
+    params,
+    searchParams,
+  ]);
+
+  if (!isLocale(rawLocale)) {
+    notFound();
+  }
+
+  if (rawLocale === defaultLocale) {
+    redirect("/");
+  }
+
+  return <HomePage locale={rawLocale} searchParams={resolvedSearchParams} />;
+}
